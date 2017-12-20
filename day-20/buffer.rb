1f1c088d2aa76bea8e5992939f4e1cd4669c62a7
Kelly Munro <kelly.munro@fluxfederation.com>
2017-12-21 10:30:55 +1300

    WIP Day 20 Part 1

--- day-20/buffer.rb
+++ day-20/buffer.rb
@@ -1,16 +1,73 @@
 class Buffer
-  attr_accessor :particle_list
+  attr_accessor :particles
 
   def initialize(particle_list)
     @particles = format_particle_list(particle_list)
   end
 
+  def move_particles
+    update_particles
+  end
+
   private
 
-  def format_particle_list(particle_list)
-    list = particle_list.split("\n")
-    list.each do |particle|
-      particle.split(', ')
+  def update_particles
+    particles.each do |particle, _|
+      update_particle_velocity(particle)
+      update_particle_position(particle)
     end
+  end
+
+  def update_particle_velocity(particle)
+    update_velocity(particle, :x)
+    update_velocity(particle, :y)
+    update_velocity(particle, :z)
+  end
+
+  def update_particle_position(particle)
+    update_position(particle, :x)
+    update_position(particle, :y)
+    update_position(particle, :z)
+  end
+
+  def update_velocity(particle, coordinate)
+    @particles[particle][:velocity][coordinate] += @particles[particle][:acceleration][coordinate]
+  end
+
+  def update_position(particle, coordinate)
+    @particles[particle][:position][coordinate] += @particles[particle][:velocity][coordinate]
+  end
+
+  def format_particle_list(particle_list)
+    particles = {}
+    list = particle_list.split("\n")
+    list.each.with_index do |particle, index|
+      particles[index] = format_particle(particle)
+    end
+
+    particles
+  end
+
+  def format_particle(particle)
+    values = particle.split(', ')
+    position = format_particle_data(values[0])
+    velocity = format_particle_data(values[1])
+    acceleration = format_particle_data(values[2])
+
+    {
+      position: position,
+      velocity: velocity,
+      acceleration: acceleration
+    }
+  end
+
+  def format_particle_data(particle_data)
+    x, y, z = particle_data[4..-2].split(',').map(&:to_i)
+
+    {
+      x: x,
+      y: y,
+      z: z
+    }
   end
 end
