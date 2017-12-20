3952ee93a72ec1fccf07eddf77e03aa047206d82
Kelly Munro <kelly.munro@fluxfederation.com>
2017-12-20 14:03:45 +1300

    WIP Day 14 Part 2

--- day-14/disk_defragmenter.rb
+++ day-14/disk_defragmenter.rb
@@ -2,7 +2,7 @@ require_relative '../day-10/ascii_knot_hash'
 
 class DiskDefragmenter
   attr_reader :key_string
-  attr_accessor :grid
+  attr_accessor :grid, :region_seed
 
   USED_SQUARE = '#'
   UNUSED_SQUARE = '.'
@@ -10,6 +10,7 @@ class DiskDefragmenter
   def initialize(key_string)
     @key_string = key_string
     @grid = []
+    @region_seed = 1
   end
 
   def calculate_disk_usage
@@ -22,11 +23,75 @@ class DiskDefragmenter
     calculate_used_squares
   end
 
+  def find_regions
+    until regions_completed?
+      print_region_seed
+      square = find_first_used_square
+      find_adjacent_squares(square[:row], square[:column])
+      require 'byebug'; byebug
+      @region_seed += 1
+    end
+
+    region_seed - 1
+  end
+
   def print_disk_usage(usage)
     puts "#{usage} squares used"
   end
 
+  def print_region_count(region_count)
+    puts "#{region_count} regions"
+  end
+
   private
+
+  def print_region_seed
+    puts "Finding squares for region: #{region_seed}"
+  end
+
+  def regions_completed?
+    grid.none? { |row| row.include?(USED_SQUARE) }
+  end
+
+  def find_first_used_square
+    grid.each.with_index do |row, row_index|
+      row.each.with_index do |column, column_index|
+        next unless used?(column)
+
+        return { row: row_index, column: column_index }
+      end
+    end
+  end
+
+  def find_adjacent_squares(row_index, column_index)
+    prior_row = row_index - 1
+    prior_column = column_index - 1
+    next_row = row_index + 1
+    next_column = column_index + 1
+
+    if next_column < grid.size
+      process_square(row_index, next_column)
+    end
+
+    if next_row < grid.size
+      process_square(next_row, column_index)
+    end
+
+    if prior_row > -1
+      process_square(prior_row, column_index)
+    end
+
+    if prior_column > -1
+      process_square(row_index, prior_column)
+    end
+  end
+
+  def process_square(row, column)
+    if used?(grid[row][column])
+      set_grid_for_position(row, column, region_seed)
+      find_adjacent_squares(row, column)
+    end
+  end
 
   def calculate_used_squares
     usage = 0
--- day-14/run.rb
+++ day-14/run.rb
@@ -1,8 +1,13 @@
 require_relative 'disk_defragmenter'
 
-input = 'oundnydw'
+input = 'flqrgnkx' #test input
+# input = 'oundnydw' #puzzle input
 
 # Part 1
 defragmenter = DiskDefragmenter.new(input)
 usage = defragmenter.calculate_disk_usage
 defragmenter.print_disk_usage(usage)
+
+# Part 2
+regions = defragmenter.find_regions
+defragmenter.print_region_count(regions)
