e2b1978731adcd8724bfdf307d1e4b521aadc956
Kelly Munro <kelly.munro@fluxfederation.com>
2017-12-22 11:31:42 +1300

    WIP Day 21 Part 1

--- day-21/fractal_art.rb
+++ day-21/fractal_art.rb
@@ -25,19 +25,86 @@ class FractalArt
     squares = if (image_size % 2).zero?
                 divide_image_into_2x2
               else
-                # divide image by 3x3
+                divide_image_into_3x3
               end
 
-    # match enhanement rule
-    # enlarge image
+    enhancements = squares.flat_map do |square|
+      patterns = find_possible_patterns_from(square)
+      patterns.select { |pattern| find_pattern_match(pattern) }
+    end
+
+    enlarge_image(enhancements)
+  end
+
+  def enlarge_image(enhancements)
+    square_size = enhancements.first.split('/').first.size
+    number_of_squares = enhancements.size
+    squares_per_side = Math.sqrt(number_of_squares).to_i
+    coordinates = determine_coordinates_for(square_size, graphic_size: squares_per_side * square_size)
+
+    enhancements.each.with_index do |enhancement, index|
+      update_image_with(enhancement, coordinates[index])
+    end
+  end
+
+  def update_image_with(enhancement, coordinate_pair)
+    rows = enhancement.split('/')
+    current_row = coordinate_pair[0]
+    current_column = coordinate_pair[1]
+
+    rows.each.with_index do |row, index|
+      cells = row.chars
+
+      cells.each do |cell|
+        @image[current_row][current_column + index] = cell
+      end
+
+      current_row += 1
+    end
   end
 
   def divide_image_into_2x2
-    number_of_squares = image_size * image_size / 4
+    coordinates = determine_coordinates_for(2)
+
+    coordinates.map do |coordinate_pair|
+      find_2x2_image(coordinate_pair[0], coordinate_pair[1])
+    end
+  end
+
+  def divide_image_into_3x3
+    coordinates = determine_coordinates_for(3)
+
+    coordinates.map do |coordinate_pair|
+      find_3x3_image(coordinate_pair[0], coordinate_pair[1])
+    end
+  end
+
+  def determine_coordinates_for(square_size, graphic_size: image_size)
+    squares_per_side = graphic_size / square_size
+    number_of_squares = squares_per_side * squares_per_side
+    coordinates = []
     current_row = 0
     current_column = 0
 
-    find_2x2_image(current_row, current_column)
+    while coordinates.size < number_of_squares
+      coordinates << [current_row, current_column]
+      current_column += square_size
+
+      if (coordinates.size % squares_per_side).zero?
+        current_row += square_size
+        current_column = 0
+      end
+    end
+
+    coordinates
+  end
+
+  def find_2x2_image(row, column)
+    "#{image[row][column]}#{image[row][column + 1]}/#{image[row + 1][column]}#{image[row + 1][column + 1]}"
+  end
+
+  def find_3x3_image(row, column)
+    "#{image[row][column]}#{image[row][column + 1]}#{image[row][column + 2]}/#{image[row + 1][column]}#{image[row + 1][column + 1]}#{image[row + 1][column + 2]}/#{image[row + 2][column]}#{image[row + 2][column + 1]}#{image[row + 2][column + 2]}"
   end
 
   def find_pattern_match(pattern)
